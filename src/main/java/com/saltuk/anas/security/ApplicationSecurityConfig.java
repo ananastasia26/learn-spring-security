740909705ba9cbcebbf9d4c2e0e0613c8afc3475
Anastasia Saltuk <saltukanas@gmail.com>
2022-10-19 23:17:19 +0600

    Add simple basic auth

---
+++ src/main/java/com/saltuk/anas/security/ApplicationSecurityConfig.java
@@ -0,0 +1,24 @@
+package com.saltuk.anas.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+//TODO: fix deprecated class
+
+@Configuration
+@EnableWebSecurity
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+        //it says "we wanna have authorize requests they have be authenticated via httpBasic
+    }
+}
