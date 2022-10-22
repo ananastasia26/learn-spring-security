f3f5a02637cb9877cb3be93bad316d6b2adf483d
Anastasia Saltuk <saltukanas@gmail.com>
2022-10-22 19:19:46 +0600

    Learn csrf and disable it for studying

--- src/main/java/com/saltuk/anas/security/ApplicationSecurityConfig.java
+++ src/main/java/com/saltuk/anas/security/ApplicationSecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 import static com.saltuk.anas.security.ApplicationUserPermission.STUDENT_WRITE;
 import static com.saltuk.anas.security.ApplicationUserRole.*;
@@ -34,21 +35,18 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
         UserDetails annasmith = User.builder()
                 .username("annasmith")
                 .password(passwordEncoder.encode("password"))
-//                .roles(STUDENT.name()) //ROLE_STUDENT
                 .authorities(STUDENT.getAuthority())
                 .build();
 
         UserDetails linda = User.builder()
                 .username("linda")
                 .password(passwordEncoder.encode("password123"))
-//                .roles(ADMIN.name()) //ROLE_ADMIN
                 .authorities(ADMIN.getAuthority())
                 .build();
 
         UserDetails tom = User.builder()
                 .username("tom")
                 .password(passwordEncoder.encode("password123"))
-//                .roles(ADMINTRAINEE.name()) //ROLE_ADMINTRAINEE
                 .authorities(ADMINTRAINEE.getAuthority())
                 .build();
 
@@ -58,6 +56,8 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+//                .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+//                .and()
                 .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/", "index", "/css/*",  "/js/*").permitAll()
