a676c78ee38a548430905c9af339ea40e9990ceb
Anastasia Saltuk <saltukanas@gmail.com>
2022-10-30 20:41:18 +0600

    Use modern approach

--- src/main/java/com/saltuk/anas/security/ApplicationSecurityConfig.java
+++ src/main/java/com/saltuk/anas/security/ApplicationSecurityConfig.java
@@ -1,65 +1,45 @@
 package com.saltuk.anas.security;
 
-import com.saltuk.anas.auth.ApplicationUserService;
 import com.saltuk.anas.jwt.JwtConfiguration;
 import com.saltuk.anas.jwt.JwtTokenVerifier;
 import com.saltuk.anas.jwt.JwtUsernameAndPasswordAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 import javax.crypto.SecretKey;
 
 import static com.saltuk.anas.security.ApplicationUserRole.STUDENT;
 
-//TODO: fix deprecated class
-
 @Configuration
 @EnableWebSecurity
-public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
-    private final PasswordEncoder passwordEncoder;
-    private final ApplicationUserService applicationUserService;
+public class ApplicationSecurityConfig {
     private final SecretKey secretKey;
     private final JwtConfiguration jwtConfiguration;
 
-    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder, ApplicationUserService applicationUserService, SecretKey secretKey, JwtConfiguration jwtConfiguration) {
-        this.passwordEncoder = passwordEncoder;
-        this.applicationUserService = applicationUserService;
+    public ApplicationSecurityConfig(SecretKey secretKey, JwtConfiguration jwtConfiguration) {
         this.secretKey = secretKey;
         this.jwtConfiguration = jwtConfiguration;
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http, AuthenticationManager authenticationManager) throws Exception {
         http
                 .csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
-                .addFilter(new JwtUsernameAndPasswordAuthenticationFilter(authenticationManager(), jwtConfiguration, secretKey))
+                .addFilter(new JwtUsernameAndPasswordAuthenticationFilter(authenticationManager, jwtConfiguration, secretKey))
                 .addFilterAfter(new JwtTokenVerifier(jwtConfiguration, secretKey), JwtUsernameAndPasswordAuthenticationFilter.class)
                 .authorizeRequests()
                 .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
                 .antMatchers("/api/**").hasRole(STUDENT.name())
                 .anyRequest()
                 .authenticated();
-    }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) {
-        auth.authenticationProvider(daoAuthenticationProvider());
-    }
-
-    @Bean
-    public DaoAuthenticationProvider daoAuthenticationProvider() {
-        var daoAuthenticationProvider = new DaoAuthenticationProvider();
-        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder);
-        daoAuthenticationProvider.setUserDetailsService(applicationUserService);
-        return daoAuthenticationProvider;
+        return http.build();
     }
 }
